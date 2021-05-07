9ab03632e3069547b8a710bd3e7b3718ad70419d
Felipe Contreras <felipe.contreras@gmail.com>
2021-05-07 03:57:28 -0500

    Move rgb_to_x256 to library
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- lib/colors.rb
+++ lib/colors.rb
@@ -11,6 +11,27 @@ def rgb_to_hex(red, green, blue)
   return '#' + (0x1000000 + rgb).to_s(16)[1..-1]
 end
 
+# From tmux
+def rgb_to_x256(*orig)
+  values = [0x00, 0x5f, 0x87, 0xaf, 0xd7, 0xff]
+
+  ir, ig, ib = orig.map do |e|
+    e < 48 ? 0 : e < 115 ? 1 : ((e - 35) / 40).to_i
+  end
+
+  average = orig.sum / 3
+  gray_index = average > 238 ? 23 : (average - 3) / 10
+  color_index = 36 * ir + 6 * ig + ib
+
+  r, g, b = orig
+  cr, cg, cb = values[ir], values[ig], values[ib]
+  gv = 8 + 10 * gray_index
+  color_err = (cr - r) ** 2 + (cg - g) ** 2 + (cb - b) ** 2
+  gray_err = (gv - r) ** 2 + (gv - g) ** 2 + (gv - b) ** 2
+
+  color_err <= gray_err ? 16 + color_index : 232 + gray_index.round
+end
+
 def hsl_to_rgb(h, s, l)
   s = s / 100.0
   l = l / 100.0
